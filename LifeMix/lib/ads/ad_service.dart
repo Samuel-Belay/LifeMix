ab5a7cd878dbb7e57fbd176dad71502d96f9a894
Kronos <152859792+Samuel-Belay@users.noreply.github.com>
2025-09-04 07:51:32 +0100

    Update ad_service.dart

--- LifeMix/lib/ads/ad_service.dart
+++ LifeMix/lib/ads/ad_service.dart
@@ -1,13 +1,10 @@
-import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 
 class AdService {
-  static Future<bool> isHuawei() async {
-    final deviceInfo = DeviceInfoPlugin();
-    if (Platform.isAndroid) {
-      final androidInfo = await deviceInfo.androidInfo;
-      return androidInfo.manufacturer?.toLowerCase() == 'huawei';
-    }
-    return false;
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+  Future<String?> getDeviceId() async {
+    AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
+    return androidInfo.id;
   }
 }
